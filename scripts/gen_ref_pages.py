20735df602f8c40a97c4d6894c3a4495bfe8786d
Jens Peder Meldgaard <jenspederm@gmail.com>
2025-12-14 22:44:31 +0100

    docs: enhance layout and docstrings
    Signed-off-by: Jens Peder Meldgaard <jenspederm@gmail.com>

--- scripts/gen_ref_pages.py
+++ scripts/gen_ref_pages.py
@@ -9,18 +9,10 @@ for path in sorted([f for f in src.rglob("*.py")]):
     module_path = path.relative_to(src).with_suffix("")
     module_name = module_path.name
     identifier = ".".join([part for part in module_path.parts if part != "__init__"])
-    docname = module_name if module_name != "__init__" else module_path.parent.name
-    mkdocs_gen_files.log.info(
-        f"{module_path}: Generating reference page for {identifier} - {docname}"
-    )
-
-    doc_path = (module_path.parent / docname).with_suffix(".md")
+    doc_path = (module_path.parent / module_name).with_suffix(".md")
     full_doc_path = Path("reference", doc_path)
-
-    if module_path.name == "__main__":
+    if module_path.name == "__main__" or module_name == "__init__":
         continue
-
     with mkdocs_gen_files.open(full_doc_path, "w") as fd:
         print("::: " + identifier, file=fd)
-
     mkdocs_gen_files.set_edit_path(full_doc_path, path.relative_to(root))
